e76d22e7abecdcd4421bc0986e6357b8a99d5621
subrata <subrata@appsmith.com>
2025-03-03 17:26:41 +0600

    Add J11 and use J10

---
+++ modules/JSModule11/jsobjects/JSModule11/JSModule11.js
@@ -0,0 +1,14 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+		return JSModule10.myFun1()
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+		return JSModule10.myFun2()
+	}
+}
